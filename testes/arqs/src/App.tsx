ab521e8eb6447fc35c50630bd5d7030e23343b0f
Vitin-Ps <vicssoares2802@gmail.com>
2024-02-07 10:34:47 -0300

    ..

---
+++ testes/arqs/src/App.tsx
@@ -0,0 +1,19 @@
+// import {NativeBaseProvider, StatusBar, Text} from 'native-base';
+// import {Temas} from './estilos/tema';
+// import {SafeAreaView} from 'react-native';
+// // import Rotas from './Rotas';
+
+import { SafeAreaView, Text } from "react-native";
+
+export default function App() {
+  return (
+    // <NativeBaseProvider theme={Temas}>
+    //   <StatusBar backgroundColor={Temas.colors.roxo.normal} />
+    //     {/* <Text mt={10}>Teste</Text> */}
+    //     {/* <Rotas /> */}
+    // </NativeBaseProvider>
+    <SafeAreaView>
+      <Text>oi</Text>
+    </SafeAreaView>
+  );
+}
